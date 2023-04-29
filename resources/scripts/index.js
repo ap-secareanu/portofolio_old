78699f3bf9ad4cc7c696edaacd656a46eea69af1
Alex Secareanu <secareanu.alexandru98@gmail.com>
2023-04-29 19:03:28 +0300

    finished first version of the project. will deploy now and test further

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,5 +1,82 @@
 $(document).ready(function() {
+
+    // Scroll Fade in animation
+
+    let screenHeight = window.innerHeight;
+
+
+    $(window).on('scroll', function() {
+
+        let currentPos = $(window).scrollTop();
+        
+        if(currentPos >= 700 && screenHeight <= 1000) {
+            $('#about').addClass('show');
+        } else if (currentPos >= 1000 && screenHeight >= 1000) {
+            $('#about').addClass('show');
+        } else {
+            $('#about').removeClass('show');
+        }
+
+        if(currentPos >= 962 && screenHeight <= 1000) {
+            $('#exp').addClass('show');
+        } else if (currentPos >= 1322 && screenHeight >= 1000) {
+            $('#exp').addClass('show');
+        } else {
+            $('#exp').removeClass('show');
+        }
+
+        if(currentPos >= 2300 && screenHeight <= 1000) {
+            $('#edu').addClass('show');
+        } else if (currentPos >= 2500 && screenHeight >= 1000) {
+            $('#edu').addClass('show');
+        } else {
+            $('#edu').removeClass('show');
+        }
+
+        if(currentPos >= 3040 && screenHeight <= 1000) {
+            $('#contact').addClass('show');
+        } else if (currentPos >= 3140 && screenHeight >= 1000) {
+            $('#contact').addClass('show');
+        } else {
+            $('#contact').removeClass('show');
+        }
+
+
+    })
+    // Scroll Fade in animation
+
+
+
+    // Mobile menu
+    $('.hamburger').click(function() {
+        if($('.hamburger').hasClass('is-active')) {
+            $('.hamburger').removeClass('is-active');
+            $('#nav_mobile').removeClass('come');
+            $('body').css('overflow-y', 'scroll');
+            $('.overlay').css('display', 'none')
+        } else {
+            $('.hamburger').addClass('is-active');
+            $('#nav_mobile').addClass('come');
+            $('body').css('overflow-y', 'hidden')
+            $('.overlay').css('display', 'inline-block')
+        }
+    })
+    // Mobile menu
+    $('#nav_mobile a').click(function() {
+        if($('.hamburger').hasClass('is-active')) {
+            $('.hamburger').removeClass('is-active');
+            $('#nav_mobile').removeClass('come');
+            $('body').css('overflow-y', 'scroll');
+            $('.overlay').css('display', 'none')
+        } else {
+            $('.hamburger').addClass('is-active');
+            $('#nav_mobile').addClass('come');
+            $('body').css('overflow-y', 'hidden')
+            $('.overlay').css('display', 'inline-block')
+        }
+    })
     
+
     // Heading Dot animation
     const showDot = () => {
         $('.heading_dot').removeClass('hide')
